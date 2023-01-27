f7d77c7eaf2e9b36dd389c777c02f591dc9e54b2
Shabnoor Parveen <114005024+ShabnoorParveen@users.noreply.github.com>
2023-01-27 22:11:04 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,10 @@
 import React from 'react'
-const Post = ({body, title}) => {
+const Post = () => {
     return (
         <div className="post">
-            <h3 className='title'>{title}</h3>
-            <p className='body'>{body}</p>
+
         </div>
     )
 }
 
-export default Post
+export { Post }
